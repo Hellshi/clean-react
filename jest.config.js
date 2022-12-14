f908f6e69e0b092ed7fa9c20f6668f37907bb05f
hellshi <hcslimaa@gmail.com>
2022-12-14 16:45:06 -0300

    test: is husky ok?

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  roots: ['<rootDir>/src'],
+  roots: ['./src'],
   collectCoverageFrom: [
-    '<rootDir>/src/**/*.{ts,tsx}'
+    './src/**/*.{ts,tsx}'
   ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',

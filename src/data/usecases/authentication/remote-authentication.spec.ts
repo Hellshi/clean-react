0b0660fae83ddc7dc3508bfd3b87474efe25c54f
hellshi <hcslimaa@gmail.com>
2022-12-14 10:20:41 -0300

    feat: ensure remote authentication calls correct url

---
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -0,0 +1,19 @@
+import { HttpPostClient } from '@/data/protocols/http/http-post-client'
+import { RemoteAuthentication } from './romote-authentication'
+
+class HttpClientPostSpy implements HttpPostClient {
+  url?: string
+  async post (url: string): Promise<void> {
+    this.url = url
+  }
+}
+
+describe('Remote Authentication', () => {
+  test('Should call HttpPostClient with the correct URL', async () => {
+    const url = 'any_url'
+    const httpClientPostSpy = new HttpClientPostSpy()
+    const sut = new RemoteAuthentication(url, httpClientPostSpy)
+    await sut.auth()
+    expect(httpClientPostSpy.url).toBe(url)
+  })
+})
